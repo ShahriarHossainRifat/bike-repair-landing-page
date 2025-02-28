2ec3c9631f0a83599c730f4cb653ca5755dd3c85
Shahriar Hossain Rifat <shahriarrifat0@gmail.com>
2025-02-28 16:36:51 +0600

    GH page deployment fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/bike-repair/", // Replace with your repo name
+  base: "/bike-repair-landing-page/", // Replace with your repo name
 });
